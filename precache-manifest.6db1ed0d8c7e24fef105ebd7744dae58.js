836320d961a94731249ef7a076438776a4ca6469
Valentyn <valentyn28101993@ukr.net>
2020-05-04 01:43:54 +0300

    Updates

---
+++ precache-manifest.6db1ed0d8c7e24fef105ebd7744dae58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e5f6143aff2d019a99e2e292526aacc",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "0716e8e859a2d0b8e69f",
+    "url": "/goit-react-hw-02-feedback/static/css/main.46be9fad.chunk.css"
+  },
+  {
+    "revision": "3bc186aac41bdb9fc21a",
+    "url": "/goit-react-hw-02-feedback/static/js/2.7ed26bd6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.7ed26bd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0716e8e859a2d0b8e69f",
+    "url": "/goit-react-hw-02-feedback/static/js/main.f6b8cdf2.chunk.js"
+  },
+  {
+    "revision": "41a0a366245a6f604f38",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.33a58230.js"
+  }
+]);
